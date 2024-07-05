433482532ca367b2bc6f7de8f32333c6a5d78217
Ethan <ecb266@nau.edu>
2024-07-05 16:39:30 -0700

    fix token error

--- app/Main.tsx
+++ app/Main.tsx
@@ -17,7 +17,7 @@ export default function Home({ posts }) {
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <p>_ site under construction _</p>
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            > Hello
+            &gt; Hello
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -27,9 +27,9 @@ export default function Home({ posts }) {
 
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-        <h4 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            > Latest
-        </h4>
+          <h4 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            &gt; Latest
+          </h4>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
